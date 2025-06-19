ce40efabd5bc0e8fddb59457e5b2652be1d3ed70
Paola Kloch <kloch.157@gmail.com>
2025-06-19 18:36:35 -0300

    fix: remove test placeholder from product header component

--- src/app/[slug]/menu/[productId]/components/product-header.tsx
+++ src/app/[slug]/menu/[productId]/components/product-header.tsx
@@ -12,7 +12,6 @@ interface ProductHeaderProps {
 export const ProductHeader = ({ product }: ProductHeaderProps) => {
 
     const { back } = useRouter()
-    test
     return (
         <div className="relative w-full h-[300px]">
             <div className="relative h-[250px] w-full">
